fa30ad93263f17ec49e0a333ce4d94876c53ed1f
Ab30657 <amar_ronaldo30657@yahoo.com>
2020-10-11 17:14:55 +0545

    Added dependencies

---
+++ BLL/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public UserType UserType { get; set; }
+    }
+
+    public enum UserType
+    {
+        Student=1,
+        Admin=2
+    }
+}
